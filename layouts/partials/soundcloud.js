cb9e0467f95ddf69afa4d929d252e8a41f91cde5
Léo De Souza <43355143+de-souza@users.noreply.github.com>
2019-02-21 00:14:11 +0100

    Allow line breaks removal during minificatioin

--- layouts/partials/soundcloud.js
+++ layouts/partials/soundcloud.js
@@ -1,11 +1,11 @@
 for (const wrapper of document.querySelectorAll(".soundcloud")) {
-  const button = document.createElement("span")
-  button.style.cursor = "pointer"
-  button.innerHTML = "Show player"
+  const button = document.createElement("span");
+  button.style.cursor = "pointer";
+  button.innerHTML = "Show player";
   button.onclick = function () {
-    const id = wrapper.getAttribute("data-id")
+    const id = wrapper.getAttribute("data-id");
     wrapper.innerHTML = `<iframe style="border: none;" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${id}&show_comments=false" width="100%" height="166"></iframe>`
-  }
-  wrapper.appendChild(button)
+  };
+  wrapper.appendChild(button);
   wrapper.insertAdjacentHTML("beforeend",' (<a href="https://soundcloud.com/pages/cookies">cookie policy</a>)')
 }

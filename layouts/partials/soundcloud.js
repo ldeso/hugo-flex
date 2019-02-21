8ba0579562e1d7c3f03fd180e5f3e98008782aa5
Léo De Souza <43355143+de-souza@users.noreply.github.com>
2019-02-21 18:38:24 +0100

    Improve style of embedded player

--- layouts/partials/soundcloud.js
+++ layouts/partials/soundcloud.js
@@ -1,7 +1,7 @@
 for (const wrapper of document.querySelectorAll(".soundcloud")) {
   const id = wrapper.getAttribute('data-id');
   wrapper.className = "soundcloud--wrapper";
-  wrapper.innerHTML = '<div class="soundcloud--player">Show player (<a href="https://soundcloud.com/pages/cookies">cookie policy</a>)</div>';
+  wrapper.innerHTML = '<div class="soundcloud--player">Soundcloud player (<a href="https://soundcloud.com/pages/cookies">cookie policy</a>)</div>';
   wrapper.onclick = function () {
     wrapper.innerHTML = `<iframe style="border:none;width:100%;" height="166" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${id}&show_comments=false"></iframe>`
   }

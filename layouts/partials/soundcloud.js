22da2618519eb19041749df092e4c67a729e4518
Léo De Souza <43355143+de-souza@users.noreply.github.com>
2019-02-25 01:55:00 +0100

    Use Suit CSS naming convention

--- layouts/partials/soundcloud.js
+++ layouts/partials/soundcloud.js
@@ -1,6 +1,6 @@
-for (const wrapper of document.querySelectorAll(".soundcloud")) {
+for (const wrapper of document.querySelectorAll(".Soundcloud")) {
   const iframe = document.createElement("iframe");
-  iframe.className = "soundcloud--iframe";
+  iframe.className = "Soundcloud-iframe";
   iframe.setAttribute("src", `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${wrapper.getAttribute('data-id')}&show_comments=false`);
 
   const link = document.createElement("a");
@@ -10,13 +10,13 @@ for (const wrapper of document.querySelectorAll(".soundcloud")) {
   link.onkeydown = e => e.stopPropagation();
 
   const player = document.createElement("div");
-  player.className = "soundcloud--player";
+  player.className = "Soundcloud-player";
   player.setAttribute("tabindex", "0");
   player.textContent = 'Load player';
   player.appendChild(link);
   player.onclick = () => { wrapper.replaceChild(iframe, player) }
   player.onkeydown = e => { if (e.keyCode == 13) wrapper.replaceChild(iframe, player) }
 
-  wrapper.className = "soundcloud--wrapper";
+  wrapper.className = "Soundcloud-wrapper";
   wrapper.appendChild(player)
 }

4e4c2dfb4ceb4d67f6503eaad588bd30e2ead29e
Léo De Souza <43355143+de-souza@users.noreply.github.com>
2019-03-06 23:55:54 +0100

    Use CSS selector instead of extra class and JS

--- assets/js/soundcloud.js
+++ assets/js/soundcloud.js
@@ -2,13 +2,11 @@ for (const wrapper of document.querySelectorAll(".Soundcloud")) {
   const iframe = document.createElement("iframe");
   iframe.className = "Soundcloud-iframe";
   iframe.setAttribute("src", `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${wrapper.getAttribute('data-id')}&show_comments=false`);
-
   const link = document.createElement("a");
   link.setAttribute("href", "https://soundcloud.com/pages/cookies");
   link.textContent = "cookie policy";
   link.onclick = e => e.stopPropagation();
   link.onkeydown = e => e.stopPropagation();
-
   const player = document.createElement("div");
   player.className = "Soundcloud-player";
   player.setAttribute("tabindex", "0");
@@ -16,7 +14,5 @@ for (const wrapper of document.querySelectorAll(".Soundcloud")) {
   player.appendChild(link);
   player.onclick = () => { wrapper.replaceChild(iframe, player) };
   player.onkeydown = e => { if (e.keyCode == 13) wrapper.replaceChild(iframe, player) };
-
-  wrapper.className = "Soundcloud-wrapper";
-  wrapper.appendChild(player)
+  wrapper.appendChild(player);
 };

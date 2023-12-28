ab2eddde47d44f257952781b675b5af8f136a324
Léo de Souza <dev@leodesouza.net>
2023-12-28 13:14:37 +0100

    Display the SoundCloud URL when JS is disabled

--- assets/js/soundcloud.js
+++ assets/js/soundcloud.js
@@ -1,11 +1,15 @@
 for (const wrapper of document.getElementsByClassName("Soundcloud")) {
-  const link = wrapper.firstElementChild;
+  const url = wrapper.firstElementChild.getAttribute('href');
   const iframe = document.createElement("iframe");
   iframe.className = "Soundcloud-iframe";
-  iframe.setAttribute("src", `https://w.soundcloud.com/player/?url=${link.getAttribute('href')}&show_comments=false`);
+  iframe.setAttribute("src", `https://w.soundcloud.com/player/?url=${url}&show_comments=false`);
   const button = document.createElement("button");
-  button.className = "Soundcloud-player";
-  button.textContent = 'Load SoundCloud player';
+  button.className = "Soundcloud-box Soundcloud-box--button";
+  button.textContent = "Load SoundCloud player";
   button.onclick = () => wrapper.replaceChildren(iframe);
-  wrapper.replaceChild(button, link);
+  const policy = document.createElement("a");
+  policy.className = "Soundcloud-policy";
+  policy.textContent = "(cookie policy)";
+  policy.setAttribute("href", "https://soundcloud.com/pages/cookies");
+  wrapper.replaceChildren(button, policy);
 };
